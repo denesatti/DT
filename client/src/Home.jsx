11ba331764d004aa4e0d998df62d93c13d9c9961
denesatti <thomasdenes47@gmail.com>
2024-06-01 23:48:33 +0300

    images now are saved in the db

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,36 +1,43 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
-
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     axios.get('http://localhost:3001/getposts')
-    .then(posts => {
-      setPosts(posts.data)
-    })
-    .catch(err => console.log(err))
-  }, [])
+      .then(response => {
+        setPosts(response.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
 
   return (
     <div className='posts_container'>
       {
         posts.map(post => (
-          <Link to={`/post/${post._id}`} className='post'> 
-          
-          <img src={`http://localhost:3001/Images/${post.file}`} alt="" />
-          <div className='post_text'>
-            <h2>{post.title}</h2>
-            <p>{post.description}</p>
-          </div>
-          
+          <Link to={`/post/${post._id}`} className='post' key={post._id}> 
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <img src={post.file} alt={post.title} />
+                  </td>
+                  <td>          
+                    <div className='post_text'>
+                      <h2>{post.title}</h2>
+                      <p>{post.description}</p>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </Link>
         ))
       }
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
